bb8cbed146f1cb9179a5807b1f8f141f6b177e54
D.I. von Briesen <divonbriesen@gmail.com>
2020-01-31 13:42:58 -0500

    added basics and rearranged

--- app.py
+++ app.py
@@ -1,12 +1,38 @@
-from flask import Flask
+from flask import Flask, render_template, url_for, request, redirect
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+#/// is relative path, //// is absolute path
+db = SQLAlchemy(app)
 
-@app.route('/')
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(200), nullable=False)
+    date_created = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self):
+        return '<Task %r>' % self.id
+
+@app.route('/', methods=['POST', 'GET'])
 def index():
-    return "Yo."
+    if request.method == 'POST':
+        task_content = request.form['content']
+        new_task = Todo(content=task_content)
+
+        try:
+            db.session.add(new_task)
+            db.session.commit()
+            return redirect('/')
+        except:
+            return "There was an issue adding your task."
+    else:
+        tasks = Todo.query.order_by(Todo.date_created).all()
+        return render_template("index.htm", tasks=tasks) 
+    #render_template knows to look in templates folder
+    #"template inheritance" - "master html file" - inherit
+    #from each other page, and change what's relevant.
 
 if __name__ == "__main__":
     app.run(debug=True)
-
-    
