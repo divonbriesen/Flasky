f5052728e681c6ac5990e5874a5d67f77637582d
D.I. von Briesen <divonbriesen@gmail.com>
2020-02-03 20:33:07 -0500

    trying to clone at home and work

--- app.py
+++ app.py
@@ -27,6 +27,7 @@ def index():
             return redirect('/')
         except:
             return "There was an issue adding your task."
+            
     else:
         tasks = Todo.query.order_by(Todo.date_created).all()
         return render_template("index.htm", tasks=tasks) 
